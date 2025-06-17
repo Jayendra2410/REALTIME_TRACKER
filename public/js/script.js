07a184728b35db266ba6d4e997e70b4885aeff5f
Jayendra2410 <jayendrasinghjadone@gmail.com>
2025-06-17 08:44:17 +0530

    final updation

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,35 @@
 const socket = io();
-
 const map = L.map("map").setView([0, 0], 15);
+
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
-        "&copy; <a href='https://www.openstreetmap.org/copyright'>BuddyWay</a> contributors",
+        "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
 }).addTo(map);
 
 const markers = {};
 let ownId = null;
 
+// Get the socket ID as soon as connected
 socket.on("connect", () => {
     ownId = socket.id;
 });
 
+function addSmallRandomOffset(value) {
+    return value + (Math.random() - 0.5) * 0.0005;
+}
+
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition(
         (position) => {
-            const { latitude, longitude } = position.coords;
-            socket.emit("send-location", { latitude, longitude });
-            if (!markers[ownId]) {
-                map.setView([latitude, longitude], 15);
+            let { latitude, longitude } = position.coords;
+            latitude = addSmallRandomOffset(latitude);
+            longitude = addSmallRandomOffset(longitude);
+            if (ownId) {
+                socket.emit("send-location", { latitude, longitude });
             }
         },
         (error) => {
-            console.log(error);
+            console.error("Geolocation error:", error);
         },
         {
             enableHighAccuracy: true,
@@ -37,9 +43,14 @@ socket.on("receive-location", (data) => {
     const { id, latitude, longitude } = data;
 
     if (!markers[id]) {
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
+        markers[id] = L.marker([latitude, longitude])
+            .addTo(map)
+            .bindTooltip(id === ownId ? "You" : `User: ${id.slice(0, 5)}`, { permanent: true });
     } else {
         markers[id].setLatLng([latitude, longitude]);
+    }
+    if (id === ownId) {
+        map.setView([latitude, longitude], 15);
     }
 });
 
